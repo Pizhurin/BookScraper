4ccc73f2f7188b68f333f8a3741c90a64b177210
Yurii Pizhurin <pizhurin@yahoo.com>
2025-11-07 17:21:51 +0300

    implementation scrape_books

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 import schedule
 import re
+import json
 from bs4 import BeautifulSoup
 
 def get_book_data(book_url: str) -> dict:
@@ -28,16 +29,30 @@ def get_book_data(book_url: str) -> dict:
     reg_price = re.compile(r"\d+(?:\.\d+)?")
     reg_available = re.compile(r"\((\d+).*?\)")
     res_dict["title"] = soup.find("title").get_text().split('|')[0].strip()
-    res_dict["price"] = re.findall(reg_price, tag_div.find("p", {"class":"price_color"}).text)[0]
+    res_dict["price"] = (
+        re.findall(reg_price, tag_div
+        .find("p", {"class":"price_color"}).text)[0]
+        )
     classes = tag_div.find_all("p")
     for next_class in classes:
         if next_class.get("class")[0] == "star-rating":
             res_dict["rating"] =next_class.get("class")[1]
             break    
-    instock_availability = tag_div.find("p", {"class":"instock availability"}).text.strip()
-    res_dict["count_available"] = re.findall(reg_available, instock_availability)[0]
-    res_dict["description"] = soup.find("meta", {"name": "description"}).get("content").strip()
-    tables_rows = soup.find("table", {"class":"table table-striped"}).find_all("tr")
+    instock_availability = (
+        tag_div.find("p", {"class":"instock availability"})
+        .text.strip()
+        )
+    res_dict["count_available"] = (
+        re.findall(reg_available, instock_availability)[0]
+        )
+    res_dict["description"] = (
+        soup.find("meta", {"name": "description"})
+        .get("content").strip()
+        )
+    tables_rows = (
+        soup.find("table", {"class":"table table-striped"})
+        .find_all("tr")
+        )
     for row in tables_rows:
         if row.find("th").text == "UPC":
             res_dict["upc"] = row.find("td").text
@@ -51,12 +66,15 @@ def get_book_data(book_url: str) -> dict:
     return res_dict
     # КОНЕЦ ВАШЕГО РЕШЕНИЯ
 
-# book_url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-# print(get_book_data(book_url))
-
-def scrape_books():
+def scrape_books(is_save=True):
     """
-    МЕСТО ДЛЯ ДОКУМЕНТАЦИИ
+    Функция для получения данных по всем книгам и записи их 
+    в файл artifacts/books_data.txt 
+    Args: 
+    is_save по-умолчанию True, определяет будет ли записаны данные 
+    в файл books_data.txt
+    Returns:
+    Возвращает список из словарей с данными по каждой книге
     """
 
     # НАЧАЛО ВАШЕГО РЕШЕНИЯ
@@ -74,14 +92,24 @@ def scrape_books():
             book_href = next_book.find("a").get("href")
             data_book = get_book_data(base_url + book_href)
             list_book.append(data_book)
-        next_href = soup.find("ul", {"class":"pager"}).find("li", {"class":"next"}).find("a").get("href")
+        next_href = (
+            soup.find("ul", {"class":"pager"})
+            .find("li", {"class":"next"})
+            .find("a").get("href")
+            )
         response = requests.get(base_url + next_href)
         soup = BeautifulSoup(response.text, "html.parser")
         count_page += 1
 
-    print(len(list_book))
+    if is_save:
+        with open("./artifacts/books_data.txt", "w", encoding="utf-8") as f:
+            json.dump(list_book, f, ensure_ascii=False, indent=2)
+
+    return list_book
     # КОНЕЦ ВАШЕГО РЕШЕНИЯ
 
 
-scrape_books()
+res = scrape_books(is_save=True) # Допишите ваши аргументы
+print(type(res), len(res)) # и проверки
+
 
